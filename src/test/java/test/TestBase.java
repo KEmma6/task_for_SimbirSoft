d60d66f656c3dae6a55c54c51745a0607e75d8df
KEmma6 <emma.chuyako@gmail.com>
2022-04-17 22:00:08 +0300

    remove empty import

--- src/test/java/test/TestBase.java
+++ src/test/java/test/TestBase.java
@@ -3,7 +3,6 @@ package test;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import java.time.Duration;
@@ -26,4 +25,5 @@ public class TestBase {
         if (driver != null)
         driver.quit();
     }
+
 }
